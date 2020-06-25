b8409aee1af242bfc09614e8c60f4983dea925d1
andelm <2315396+marcvanandel@users.noreply.github.com>
2020-06-25 10:34:00 +0200

    storing secrets with gpg encryption

---
+++ secrets.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function encrypt() {
+    gpg --yes --batch --passphrase $PASSPHRASE -c secrets.json
+}
+
+function decrypt() {
+    gpg --yes --batch --passphrase $PASSPHRASE secrets.json.gpg
+}
+
+function printHelp() {
+    echo "Usage:"
+    echo "  secrets.sh <MODE> <PASSPHRASE>"
+    echo "    <MODE>"
+    echo "      - 'encrypt' - encrypt file 'secrets.json'"
+    echo "      - 'decrypt' - decrypt file 'secrets.json'"
+    echo ""
+    echo "Examples:"
+    echo "  secrets.sh encrypt helloworld"
+    echo "  secrets.sh decrypt helloworld"
+}
+
+## Parse mode
+if [[ $# -lt 2 ]] ; then
+  printHelp
+  exit 0
+else
+  MODE=$1
+  PASSPHRASE=$2
+
+  case $MODE in
+    encrypt )
+      encrypt
+      exit 0
+      ;;
+    decrypt )
+      decrypt
+      exit 0
+      ;;
+    * )
+      printHelp
+      exit 1
+      ;;
+  esac
+fi
