d9c59bd08a67e0bd9559a890652eb97cb0eabb50
andelm <2315396+marcvanandel@users.noreply.github.com>
2020-07-07 16:30:41 +0200

    kafka via strimzi + kafka-ca secrets

--- README.md
+++ README.md
@@ -44,6 +44,16 @@ $ az account set --subscription "etc-test"
 $ ./ops.sh get-credentials -c <cluster-name> -p <passphrase (of secrets.json.gpg)>
 ```
 
+Get credentials for `gemeente-a`, `gemeente-b` and `viewer` clusters:
+
+```bash
+$ ./ops.sh get-credentials -p $SENSRNET_PASSPHRASE -c sensrnet-gemeente-a -e labs_test
+
+$ ./ops.sh get-credentials -p $SENSRNET_PASSPHRASE -c sensrnet-gemeente-b -e labs_test
+
+$ ./ops.sh get-credentials -p $SENSRNET_PASSPHRASE -c sensrnet-viewer -e labs_test
+```
+
 Select Kadaster PLS AKS test cluster:
 
 ```bash
@@ -62,12 +72,34 @@ $ az aks create --resource-group sensrnet-kafka-2 --name sensrnet-aks-kafka-2 --
 $ az aks get-credentials --resource-group sensrnet-kafka-2 --name sensrnet-aks-kafka-2
 
 $ kubectl config use-context sensrnet-aks-kafka-2
+```
 
+```bash
 $ ./kust.sh apply -f kafka -e test
 
 $ kubectl -n kafka get all
 
 5b5621aa-64e5-4b63-a3a0-a5951c0e6bda
+```
+
+Deployment using [strimzi](https://itnext.io/kafka-on-kubernetes-the-strimzi-way-part-2-43192f1dd831)
+
+```bash
+$ kubectl get configmap/my-kafka-cluster-kafka-config -o yaml > my-kafka-cluster.config.json
+
+$ export CLUSTER_NAME=my-kafka-cluster
+$ kubectl get secret $CLUSTER_NAME-cluster-ca-cert -o jsonpath='{.data.ca\.crt}' | base64 --decode > ca.crt
+$ kubectl get secret $CLUSTER_NAME-cluster-ca-cert -o jsonpath='{.data.ca\.password}' | base64 --decode > ca.password
+
+$ kubectl get service/${CLUSTER_NAME}-kafka-external-bootstrap --output=jsonpath={.status.loadBalancer.ingress[0].ip}
+```
+
+Deploy secrets - in this case: `kafka-ca`
+
+```bash
+$ ./ops.sh use-cluster -c sensrnet-gemeente-a -e labs_test
+
+$ ./deploySecret.sh kafka-ca ca.crt.gpg ca.password.gpg -p $SENSRNET_PASSPHRASE
 ```
 
 ## Local Dev Env
---
+++ deploySecret.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# default settings
+export VERBOSE=false
+
+function printHelp() {
+    echo "Usage:"
+    echo "  deploySecret.sh <name> <username-file> <password-file> [FLAGS]"
+    echo ""
+    echo "    Flags:"
+    echo "    -p <passphrase> - Passphrase of 'secrets.json' file"
+    echo "    -verbose - Print debug info"
+    echo ""
+    echo "Examples:"
+    echo "  deploySecret.sh my-secret user.txt pw.txt -p helloworld"
+}
+
+function debug() {
+  if [[ "$VERBOSE" = true ]]; then
+    echo -e "${COLOR_CYAN}  DEBUG ${*}${NC}"
+  fi
+}
+
+function error() {
+  echo -e "${COLOR_RED}ERROR ${*}${NC}"
+}
+
+function strippedFilename()
+{
+    local __resultvar=$1
+    local __filename=$2
+    local __length=${#__filename}
+    if [[ "$__resultvar" ]]; then
+        eval $__resultvar=${__filename:0:$__length-4}
+    else
+        echo "ERROR: Getting filename from [$__filename]"
+    fi
+}
+
+function deploySecret() {
+    if [[ "$PASSPHRASE" == "" ]]; then
+      echo "ERROR: PASSPHRASE is empty! (use option -p)"
+      exit 1
+    fi
+
+    echo "Deploy secret [$NAME]"
+    strippedFilename USER_FILE "$USER"
+    debug "$USER_FILE"
+    ./secrets.sh decrypt $USER $PASSPHRASE > $USER_FILE
+
+    if [[ ! -f "$USER_FILE" ]]; then
+      error "$USER_FILE could not be found!"
+      exit 1
+    fi
+
+    strippedFilename PASS_FILE "$PASS"
+    debug "$PASS_FILE"
+    ./secrets.sh decrypt $PASS $PASSPHRASE > $PASS_FILE
+    echo
+
+    if [[ ! -f "$PASS_FILE" ]]; then
+      error "$PASS_FILE could not be found!"
+      exit 1
+    fi
+
+    kubectl create secret generic "$NAME" --from-file="username=./$USER_FILE" --from-file="password=./$PASS_FILE"
+
+    kubectl get secrets
+
+    echo
+    echo "Cleaning up local files ..."
+    rm $USER_FILE $PASS_FILE
+    echo
+    echo "done."
+}
+
+# colors
+COLOR_CYAN='\033[0;36m'
+COLOR_RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# properties
+
+## Parse mode
+if [[ $# -lt 3 ]] ; then
+  printHelp
+  exit 0
+else
+  NAME=$1
+  USER=$2
+  PASS=$3
+  shift 3
+fi
+
+# parse flags
+
+while [[ $# -ge 1 ]] ; do
+  key="$1"
+  case $key in
+  -h )
+    printHelp
+    exit 0
+    ;;
+  -p )
+    PASSPHRASE="$2"
+    shift
+    ;;
+  -verbose )
+    VERBOSE=true
+    shift
+    ;;
+  * )
+    echo
+    echo "Unknown flag: $key"
+    echo
+    printHelp
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+CURRENT_CONTEXT=$(kubectl config current-context)
+echo "Using current context: $CURRENT_CONTEXT"
+echo
+
+
+deploySecret
