1cece6301b959c095718430ffff3039f61da244b
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-07-22 17:30:41 +0200

    update monitoring

--- monitoring/utils/scripts/generate-k8s-mixin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Generate K8S-mixin dashboards, alerts and rules
-jsonnet -J ./kubernetes-mixin-release-0.1  -J ./kubernetes-mixin-release-0.1/vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusAlerts)' > ./alerts/alerts-k8s-mixin.yaml
-jsonnet -J ./kubernetes-mixin-release-0.1 -J ./kubernetes-mixin-release-0.1/vendor -S -e 'std.manifestYamlDoc((import "mixin.libsonnet").prometheusRules)' > ./rules/rules-k8s-mixin.yaml
-jsonnet -J ./kubernetes-mixin-release-0.1 -J ./kubernetes-mixin-release-0.1/vendor -m ./dashboards -e '(import "mixin.libsonnet").grafanaDashboards'
