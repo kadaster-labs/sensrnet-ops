b92c07fd6585dbac515f95c99544721b19a91a3b
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2021-07-16 14:09:28 +0200

    cleanup old logging and monitoring files

--- logging/README.md
+++
@@ -1,5 +0,0 @@
-# Logging
-
-We make use of the EFK stack, consisting of Elasticsearch, Fluentd and Kibana.
-
-![EFK overview](/efk-stack.png)
--- monitoring/utils/scripts/generate-custom-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Dashboards - compress all dashboards ./generated-manifests/dashboards and copy to dashboard-directory ./base/grafana/dashboards 
-for f in ./dashboards/*.json
-do
-#        echo   $(basename $f)
-#        echo $f
-        cat $f | gojsontoyaml -yamltojson > ../base/grafana/dashboards/$(basename $f)
-done
